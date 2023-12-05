da637dd0bafa603a7c03e5d6ebc04106dda83a57
Agata <9agata12@gmail.com>
2023-12-05 18:44:50 +0100

    Tworzenia adresów WWW

--- bookingcom_web/admin.py
+++ bookingcom_web/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article\
+from django.urls import path
+from bookingcom_web.views import test_response
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('test/', test_response),
+]
+
+
 # Register your models here.
 admin.site.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
