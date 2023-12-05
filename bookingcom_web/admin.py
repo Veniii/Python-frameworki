fb3c3d7e45f4e4f48ba77da48d05429898994ce2
Agata <9agata12@gmail.com>
2023-12-05 18:24:45 +0100

    Zarządzanie polami w adminie

--- bookingcom_web/admin.py
+++ bookingcom_web/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Article
 # Register your models here.
 admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ["title", "content", "year", "imgThumb"]
+    exclude = ["content"]
+    list_display = ["title", "year"]
+    list_filter = ["year"]
+    search_fields = ["title", "content"]
--- bookingcom_web/models.py
+++ bookingcom_web/models.py
@@ -4,7 +4,13 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=150, blank=False, unique=False)
+    content = models.TextField(default='')
     year = models.PositiveSmallIntegerField(default=2023)
 
 
+def __str__(self):
+    return self.title_with_year()
+
+def title_with_year(self):
+    return "{} ({})".format(self.title, self.year)
 
