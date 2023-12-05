5fdedd1f85e28a2c4a6571e4e630589fa2e42c15
Agata <9agata12@gmail.com>
2023-12-05 18:15:43 +0100

    Modal Fields

--- bookingcom_web/models.py
+++ bookingcom_web/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=150, blank=False, unique=False)
+    year = models.PositiveSmallIntegerField(default=2023)
+
 
 
